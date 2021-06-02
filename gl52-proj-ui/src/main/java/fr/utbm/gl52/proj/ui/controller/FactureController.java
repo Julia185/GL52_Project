4dcb53b0fc1908061b83094284b722f658c6e2ab
Julia <julia.rolland18@hotmail.fr>
2021-06-02 16:30:13 +0200

    Ajout Ligne Facture fxml

--- gl52-proj-ui/src/main/java/fr/utbm/gl52/proj/ui/controller/FactureController.java
+++ gl52-proj-ui/src/main/java/fr/utbm/gl52/proj/ui/controller/FactureController.java
@@ -1,5 +1,13 @@
 package fr.utbm.gl52.proj.ui.controller;
 
-public class FactureController extends AbstractController{
+import java.io.IOException;
 
+import javafx.fxml.FXML;
+
+public class FactureController extends AbstractController{
+	@FXML
+	@Override
+	public void switchToMainScene() throws IOException {
+		super.switchToMainScene();
+	}
 }
