4cc66a02c3d549d965f4ebb7b2056329563f8a25
litzler marco <marco.litzler@hotmail.com>
2021-06-11 09:47:24 +0200

    Update SAV.java

--- gl52-proj-ui/src/main/java/fr/utbm/gl52/proj/model/sav/SAV.java
+++ gl52-proj-ui/src/main/java/fr/utbm/gl52/proj/model/sav/SAV.java
@@ -1,5 +1,64 @@
 package fr.utbm.gl52.proj.model.sav;
 
+import fr.utbm.gl52.proj.model.Client;
+import fr.utbm.gl52.proj.model.sav.facture.Facture;
+
 public class SAV {
+
+	private Client client;
+	private Demande demande;
+	private Reparation reparation;
+	private Facture facture;
+
+	public SAV() {
+		super();
+	}
+
+	public SAV(Client client, Demande demande, Reparation reparation, Facture facture) {
+		super();
+		this.client = client;
+		this.demande = demande;
+		this.reparation = reparation;
+		this.facture = facture;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Demande getDemande() {
+		return demande;
+	}
+
+	public void setDemande(Demande demande) {
+		this.demande = demande;
+	}
+
+	public Reparation getReparation() {
+		return reparation;
+	}
+
+	public void setReparation(Reparation reparation) {
+		this.reparation = reparation;
+	}
+
+	public Facture getFacture() {
+		return facture;
+	}
+
+	public void setFacture(Facture facture) {
+		this.facture = facture;
+	}
+
+	@Override
+	public String toString() {
+		return " " + this.client.getNomCli() + " " + this.client.getPrenomCli().substring(0, 1)
+				+ this.client.getPrenomCli().substring(1).toLowerCase() + " | " + this.demande.getNatureRep()
+				+ " | " + this.demande.getRefProd() + " | " /*+ this.facture.getDate()*/;
+	}
 
 }
